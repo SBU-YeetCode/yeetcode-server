233e9fc933d2946e5f67eeaca4f75562edaa020a
Saransh Grover <contact@saranshgrover.com>
2021-04-15 14:14:44 -0400

    change cors config

--- src/bootstrap/loaders/express.ts
+++ src/bootstrap/loaders/express.ts
@@ -5,18 +5,23 @@ import { config, PRODUCTION } from '../../config'
 import passportConfig from './passport'
 import sessionConfig from './session'
 
-
 export default async (app: express.Application) => {
 	// Lets us accept JSON as a return from clients
 	app.use(express.json())
-
 	// Cors configuration
-	app.use('*', cors({ origin: config.clientOrigin, credentials: true }))
-
+	app.use(
+		'*',
+		cors({
+			origin: ['http://localhost:3000', config.clientOrigin],
+			credentials: true,
+		})
+	)
 	// Sets various HTTP headers to help protect our app
-	app.use(helmet({
-		contentSecurityPolicy: false
-	}))
+	app.use(
+		helmet({
+			contentSecurityPolicy: false,
+		})
+	)
 
 	await sessionConfig(app)
 
