5699b820a67f796da921cbcaa82196ff68873c93
IsaiahGz <isaiahgala99@gmail.com>
2021-04-29 03:17:41 -0400

    change comment requirement

--- src/modules/comment/service.ts
+++ src/modules/comment/service.ts
@@ -8,19 +8,24 @@ import GameProgressModel from '../gameProgress/model'
 
 @Service() // Dependencies injection
 export default class CommentService {
-	constructor(private readonly commentModel: CommentModel, private readonly gameProgressModel: GameProgressModel) {}
+	constructor(
+		private readonly commentModel: CommentModel,
+		private readonly gameProgressModel: GameProgressModel
+	) {}
 	public async getById(id: ObjectId) {
 		const comment = await this.commentModel.getById(id)
 		if (!comment) throw new Error('No comment found')
 		return comment
 	}
 
-
 	public async createComment(comment: CommentInput) {
-		const commentExists = await this.commentModel.exists({userId: comment.userId, gameId: comment.gameId})
-		if(commentExists) throw new Error('Comment already by user in game')
-		const gameProgressCompleted = await this.gameProgressModel.exists({userId: comment.userId, gameId: comment.gameId, isCompleted: true})
-		if(!gameProgressCompleted) throw new Error('User has not completed the game they are trying to write a comment for')
+		const commentExists = await this.commentModel.exists({
+			userId: comment.userId,
+			gameId: comment.gameId,
+		})
+		if (commentExists) throw new Error('Comment already by user in game')
+		// const gameProgressCompleted = await this.gameProgressModel.exists({userId: comment.userId, gameId: comment.gameId, isCompleted: true})
+		// if(!gameProgressCompleted) throw new Error('User has not completed the game they are trying to write a comment for')
 		const newComment = await this.commentModel.createComment(comment)
 		if (!newComment) throw new Error('Unable to create comment')
 		return newComment
